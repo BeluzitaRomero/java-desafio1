1ca451eb643ae7b904c10f05ea6844862791a494
Belen Romero <romero.abelen@gmail.com>
2024-02-22 17:05:50 -0300

    desafio 1

---
+++ src/main/java/com/coderhouse/Variables.java
@@ -0,0 +1,16 @@
+package com.coderhouse;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float numero = 1.65f;
+
+		int numeroEntero = (int) numero;
+		float porcionFlotante = numero - numeroEntero;
+
+		System.out.println("Numero: " + numero + " - Parte entera: " + numeroEntero + " - Parte decimal:  " + porcionFlotante);
+
+	}
+
+}
